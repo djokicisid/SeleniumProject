5a43e34d0ae0effcda06ad2dd6620c058ff366a3
Isidora Djokic <isidora.djokic@gmail.com>
2024-01-30 15:33:09 +0000

    Resolve 5 "gitignore + POP"

--- src/test/java/LoginTest.java
+++ src/test/java/LoginTest.java
@@ -1,26 +1,25 @@
 
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
 public class LoginTest {
+    private LoginPage loginPage;
 
     @Test
     public void validLogin() {
 
         WebDriver driver = new ChromeDriver();
+        loginPage = new LoginPage(driver);
         driver.get("https://the-internet.herokuapp.com/login");
 
-        driver.findElement(By.id("username")).sendKeys("tomsmith");
-        driver.findElement(By.id("password")).sendKeys("SuperSecretPassword!");
-
-        driver.findElement(By.cssSelector("button[type='submit']")).click();
+        loginPage.enterUsername("tomsmith");
+        loginPage.enterPassword("SuperSecretPassword!");
+        loginPage.clickLoginButton();
 
         if (driver != null) {
             driver.quit();
         }
     }
 }
+
