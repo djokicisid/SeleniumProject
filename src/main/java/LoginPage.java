5a9833b87d57c3695538a867319cdb2cf1b9c129
Isidora Djokic <isidora.djokic@gmail.com>
2024-01-31 10:40:55 +0000

    Resolve "Abstract class, docks and logger"

--- src/main/java/LoginPage.java
+++ src/main/java/LoginPage.java
@@ -1,6 +1,27 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
+/**
+ * This class represents a page object for handling login functionality. It is written using the Page Object design pattern.
+ * This class exposes methods that represent actions that can be performed on the login web page.
+ * Elements on the page (e.g., buttons, text fields) are encapsulated within the page class.
+ *
+ * This class encapsulates the interactions and elements related to the login page,
+ * providing methods to enter credentials, click the login button, and perform login-related actions.
+ * It is designed to be used in conjunction with Selenium WebDriver for browser automation.
+ *
+ * Example usage:
+ * <pre>{@code
+ * LoginPage loginPage = new LoginPage(driver);
+ * loginPage.enterUsername("exampleUser");
+ * loginPage.enterPassword("securePassword");
+ * loginPage.clickLoginButton();
+ * }</pre>
+ *
+ * Note: This class assumes that the underlying WebDriver instance (driver) is properly initialized.
+ *
+ * @see <a href="https://www.selenium.dev/documentation/test_practices/encouraged/page_object_models/">Page Object models documentation</a>
+ */
 public class LoginPage {
     private WebDriver driver;
     private By usernameField = By.id("username");
@@ -8,22 +29,54 @@ public class LoginPage {
     private By loginButton = By.cssSelector("button[type='submit']");
     private By errorMessage = By.cssSelector(".error-message");
 
+    /**
+     * This method is the constructor for the LoginPage class.
+     * Constructs a new LoginPage object with the specified WebDriver instance.
+     * <p>
+     * This constructor initializes the LoginPage with the provided WebDriver instance.
+     * </p>
+     * @param driver The WebDriver instance used for browser automation.
+     */
     public LoginPage(WebDriver driver) {
         this.driver = driver;
     }
 
+    /**
+     * This method enters the username data into web element for usernameField
+     * <p>
+     * This method simulates entering the provided username into the username input field.
+     * </p>
+     * @param username The username to be entered.
+     */
     public void enterUsername(String username) {
         driver.findElement(usernameField).sendKeys(username);
     }
 
+    /**
+     * This method enters password data into web element for passwordField
+     * <p>
+     * This method simulates entering the provided password into the password input field.
+     * </p>
+     * @param password The password to be entered.
+     */
     public void enterPassword(String password) {
         driver.findElement(passwordField).sendKeys(password);
     }
 
+    /**
+     * This method is used to simulate a user clicking on a Login button element on a web page.
+     * <p>
+     * This method simulates clicking the login button to submit the login form.
+     * </p>
+     */
     public void clickLoginButton() {
         driver.findElement(loginButton).click();
     }
 
+    /**
+     * This method his used to retrieve the text content of the error message web element.
+     * @return Error Message text
+     */
     public String getErrorMessage() {
         return driver.findElement(errorMessage).getText();
     }
--- src/test/java/LoginTest.java
+++ src/test/java/LoginTest.java
@@ -1,25 +1,39 @@
-
+import Utils.WebDriverHandler;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 
-public class LoginTest {
+/**
+ * The LoginTest class contains test cases for the login functionality of the application.
+ * <p>
+ * This class includes test method to validate the login process.
+ * It uses Selenium WebDriver for browser automation to simulate user interactions.
+ * </p>
+ */
+public class LoginTest extends WebDriverHandler {
     private LoginPage loginPage;
 
+    /**
+     * Tests the login functionality with valid credentials.
+     * <p>
+     * This method initializes the LoginPage, navigates to the login page,
+     * enters valid username and password, clicks the login button,
+     * and logs information if the login is successful.
+     * </p>
+     * <p>
+     * Preconditions:
+     * The WebDriver instance (driver) must be initialized.
+     * The LoginPage class must be set up and properly implemented.
+     * </p>
+     * @see LoginPage
+     */
     @Test
     public void validLogin() {
-
-        WebDriver driver = new ChromeDriver();
         loginPage = new LoginPage(driver);
         driver.get("https://the-internet.herokuapp.com/login");
 
         loginPage.enterUsername("tomsmith");
         loginPage.enterPassword("SuperSecretPassword!");
         loginPage.clickLoginButton();
-
-        if (driver != null) {
-            driver.quit();
-        }
+        LOGGER.info("Login successful.");
     }
 }
 
